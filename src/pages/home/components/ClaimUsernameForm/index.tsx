09fe7a3073d632ad0498c49ecad96506ba630eb2
Wallace Júnior <wflj1997@gmail.com>
2022-12-24 15:56:19 -0300

    Utilizando React Hook Form

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,16 +1,43 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Button, TextInput } from "@ignite-ui/react";
 import { ArrowRight } from "phosphor-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import { Form } from "./styles";
+
+const claimUsernameFormSchema = z.object({
+  username: z.string(),
+});
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>;
 
 interface ClaimUsernameFormProps {}
 
-export const ClaimUsernameForm: React.FC<ClaimUsernameFormProps> = () => (
-  <Form as="form" onSubmit={event => event.preventDefault()}>
-    <TextInput size="sm" prefix="ignite.com/" placeholder="seu-usuario" />
+export const ClaimUsernameForm: React.FC<ClaimUsernameFormProps> = () => {
+  const { register, handleSubmit } = useForm<ClaimUsernameFormData>({
+    resolver: zodResolver(claimUsernameFormSchema),
+    values: {
+      username: "",
+    },
+  });
 
-    <Button size="sm" type="submit">
-      Reservar
-      <ArrowRight />
-    </Button>
-  </Form>
-);
+  const handleClaimUsername = handleSubmit(values => {
+    console.log(values);
+  });
+
+  return (
+    <Form as="form" onSubmit={handleClaimUsername}>
+      <TextInput
+        size="sm"
+        prefix="ignite.com/"
+        placeholder="seu-usuario"
+        {...register("username")}
+      />
+
+      <Button size="sm" type="submit">
+        Reservar
+        <ArrowRight />
+      </Button>
+    </Form>
+  );
+};
